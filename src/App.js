561b74a04ef6b88e3a046188b65563fbe4d86974
Ritik Kumar <srivastavaritik360@gmail.com>
2021-12-03 23:36:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Let's get started!</h1>
+      <p>chaliye shuru karte hai!</p>
+    </div>
+  );
+}
+
+export default App;
