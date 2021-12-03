790ce10b57db707e8c4fef98e8cb6b57b964f79b
Ritik Kumar <srivastavaritik360@gmail.com>
2021-12-03 23:44:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h1>Let's get started!</h1>
       <p>chaliye shuru karte hai!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,6 @@
+import React from "react";
+
 function ExpenseItem() {
     return (<h1>Expense Items here!</h1>)
 }
+export default ExpenseItem;
