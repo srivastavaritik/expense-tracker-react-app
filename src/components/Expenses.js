c25a268f43f647b543db701fcccdf2a40e194810
Ritik Kumar <srivastavaritik360@gmail.com>
2021-12-05 14:36:58 +0530

    .

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import './CSS/Expenses.css'
+import Card from "./Card";
 
 function Expenses(props) {
     const expenses = props.expenses;
   return (
-    <div  className='expenses'>
+    <Card  className='expenses'>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -26,7 +27,7 @@ function Expenses(props) {
         amount={expenses[3].amount}
         date={expenses[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
