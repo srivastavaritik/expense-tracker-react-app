8d7124be8b3b201a601f73429b1074345810bd99
Ritik Kumar <srivastavaritik360@gmail.com>
2021-12-05 13:48:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
-
+import Expenses from "./components/Expenses";
 function App() {
   const expenses = [
     {
@@ -25,12 +24,9 @@ function App() {
   ];
 
   return (
-    <div>
+    <div >
       <h1>Expense Tracker</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
