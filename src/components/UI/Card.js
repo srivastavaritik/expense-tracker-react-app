6919f565f46af33d88af5640e28923ea56271b66
Ritik Kumar <srivastavaritik360@gmail.com>
2021-12-05 15:20:56 +0530

    .

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../CSS/Card.css";
 
-function Card(props) { 
+const Card = (props) => { 
   const classes = 'card '+ props.className;
     return (
     <div className={classes}>{props.children}</div>
