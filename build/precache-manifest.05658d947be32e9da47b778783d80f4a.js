7841fefc5e7831e548e1401f50664eaadad03cce
Ritik Kumar <srivastavaritik360@gmail.com>
2021-12-11 14:42:20 +0530

    react build

---
+++ build/precache-manifest.05658d947be32e9da47b778783d80f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f22f51e12f64e8f972c9693b1e07c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9889ad3cd902231e33e",
+    "url": "/static/css/main.8d2edfda.chunk.css"
+  },
+  {
+    "revision": "c46b8b1ff164d25b64da",
+    "url": "/static/js/2.d6818657.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.d6818657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9889ad3cd902231e33e",
+    "url": "/static/js/main.ec5eb09f.chunk.js"
+  },
+  {
+    "revision": "5132f4db0caab9ba715b",
+    "url": "/static/js/runtime-main.30538d72.js"
+  }
+]);
